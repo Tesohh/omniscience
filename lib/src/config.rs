f7da2259a6d73cc8204c64688387252ce5f7ed93
Simone Tesini <simozilla006@gmail.com>
2025-12-26 17:09:01 +0100

    add project root resolution

--- lib/src/config.rs
+++ lib/src/config.rs
@@ -1,6 +1,13 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
+use miette::Diagnostic;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+static OMNI_TOML: &str = "omni.toml";
 
 /// config contained in `omni.toml`,
 /// which also counts as project root.
@@ -20,6 +27,39 @@ pub struct Project {
     pub build_path: PathBuf,
 }
 
+#[derive(Error, Debug, Diagnostic)]
+pub enum Error {
+    #[error("the pwd provided is not a directory. the developer did something wrong!")]
+    PwdIsAFile,
+    #[error("no project root found")]
+    #[diagnostic(help("try executing this command in a omniscience project directory"))]
+    NoProjectRoot,
+    #[error("io error: {0}")]
+    IOError(#[from] std::io::Error),
+}
+
+/// returns the parent directory that contains omni.toml, if it's found, otherwise `Error::NoProjectRoot`
+pub fn find_project_root(pwd: impl AsRef<Path>) -> Result<PathBuf, Error> {
+    let mut current = pwd.as_ref().canonicalize()?;
+    if current.is_file() {
+        return Err(Error::PwdIsAFile);
+    }
+
+    loop {
+        let target = current.join(OMNI_TOML);
+        if target.exists() {
+            return Ok(current);
+        }
+
+        match current.parent() {
+            Some(parent) if parent != Path::new("") => current = parent.to_path_buf(),
+            _ => break,
+        }
+    }
+
+    Err(Error::NoProjectRoot)
+}
+
 fn default_nodes_path() -> PathBuf {
     PathBuf::from("nodes.toml")
 }
@@ -30,6 +70,8 @@ fn default_build_path() -> PathBuf {
 
 #[cfg(test)]
 mod tests {
+    use tempfile::tempdir;
+
     use super::*;
 
     #[test]
@@ -77,5 +119,39 @@ mod tests {
                 dir_aliases: HashMap::new()
             }
         )
+    }
+
+    #[test]
+    fn test_project_root_resolution_fail() {
+        let mut path = tempdir().unwrap().path().to_path_buf();
+        path.push("notes/linalg/deep/nested");
+
+        std::fs::create_dir_all(&path).unwrap();
+        // just create the path, but never add omni.toml
+
+        assert!(matches!(
+            find_project_root(&path),
+            Err(Error::NoProjectRoot)
+        ));
+    }
+
+    #[test]
+    fn test_project_root_resolution() {
+        let root = tempdir().unwrap().path().canonicalize().unwrap();
+        let mut path = root.clone();
+        path.push("notes/linalg/deep/nested");
+
+        std::fs::create_dir_all(&path).unwrap();
+        std::fs::File::create(root.join(OMNI_TOML)).unwrap();
+
+        assert_eq!(find_project_root(&path).unwrap(), root);
+    }
+
+    #[test]
+    fn test_project_root_resolution_same_path() {
+        let temp = tempdir().unwrap(); // needs to be given a binding or else it would be dropped
+        let root = temp.path().canonicalize().unwrap();
+        std::fs::File::create(root.join(OMNI_TOML)).unwrap();
+        assert_eq!(find_project_root(&root).unwrap(), root);
     }
 }
